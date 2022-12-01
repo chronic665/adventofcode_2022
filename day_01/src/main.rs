31778d1e9462850c46d18d9a5382246dfd39d7a7
chronic <chronic6666@gmail.com>
2022-12-01 21:45:54 +0000

    day 1 - exercise 2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -12,8 +12,7 @@ fn main() -> std::io::Result<()> {
         .expect("file not found!");
     let  buf_reader = BufReader::new(file);
 
-    let mut most_calories: u64 = 0;
-    let mut elf_number = 1;
+    let mut top_elves: Vec<u64> = vec![];
     let mut current_elf: Vec<u64> = vec![];
     for l in buf_reader.lines() {
         let line = l.unwrap();
@@ -22,19 +21,15 @@ fn main() -> std::io::Result<()> {
             current_elf.push(calory);
         } else {
             let elf_calories = sum(&current_elf);
-            if elf_calories > most_calories {
-                most_calories = elf_calories;
-                println!("Elf {} has the most calories so far: {}", elf_number, most_calories)
-            } else if elf_calories == most_calories {
-                println!("Elf {} matches the maximum calories", elf_number);
-            }
+            top_elves = evaluate_calories(elf_calories, top_elves);
 
-            elf_number += 1;
             current_elf = vec![];
         }
     }
-    println!();
-    println!("Most calories: {}", most_calories);
+    println!("~~~~~~");
+    println!("Top Elves: ");
+    let total = sum_and_print(&top_elves);
+    println!("Total: {}", total);
     Ok(())
 }
 
@@ -44,4 +39,39 @@ fn sum(elf: &Vec<u64>) -> u64 {
         result += calory;
     }
     return result;
+}
+
+fn evaluate_calories(elf_calories: u64, mut top_elves: Vec<u64>) -> Vec<u64> {
+    // ordering from highest (index 0) to lowest (index 2)
+    for (i, top_calories) in top_elves.iter().enumerate() {
+        // new elf has more calories than the current one 
+        if elf_calories > *top_calories {
+            let mut new_top_elves = top_elves[0..i].to_vec();
+            let tail = top_elves[i..top_elves.len()-1].to_vec();
+            
+            new_top_elves.push(elf_calories);
+            new_top_elves.extend(tail);
+
+            println!("Elf is in top 3 of elves so far: rank {} with {} calories", i, elf_calories);
+            println!("\tnew top_elves:");
+            sum_and_print(&new_top_elves);
+            return new_top_elves;
+        }
+    }
+
+    // initially filling up the top spots
+    if top_elves.len() < 3 {
+        println!("Elf is in top 3 of elves so far: rank {} with {} calories", top_elves.len(), elf_calories);
+        top_elves.push(elf_calories);
+    }
+    return top_elves;
+}
+
+fn sum_and_print(top_elves: &Vec<u64>) -> u64{
+    let mut total = 0;
+    for elf in top_elves {
+        println!("\tcalories: {}", elf);
+        total += elf;
+    }
+    return total;
 }
